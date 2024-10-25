ab8ce48096ea0a2b1c6c82988d8b747ac944fadb
MarouaBenAli2024 <benalimaroua132@gmail.com>
2024-10-25 09:29:21 +0100

    first commit

---
+++ read-file.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const path = require("path");
+const filePath=path.resolve("Content","welcome.txnodet")
+const data = fs.readFileSync(filePath, { encoding: "utf8" });
+console.log(data)
